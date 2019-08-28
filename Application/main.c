a4f2b4a586c88c1d3fa21f48dc09a38f91d19f9b
SK Dubey <shubham.kant.dubey@gmail.com>
2019-08-28 21:22:00 +0530

    [ASGMT_2] Create thread stack pointer.
        1. Create two stack and stack pointer.
        2. Halt at systic timer and pass created stack address.
        3. So CPU will use new stack area for contex.

--- Application/main.c
+++ Application/main.c
@@ -41,28 +41,47 @@ int green_main(void);
 volatile uint32_t tick;
 volatile uint32_t _tick;
 
+uint32_t green_stack[40];
+uint32_t red_stack[40];
+
+uint32_t *sp_green = &green_stack[40];
+uint32_t *sp_red   = &red_stack[40];
+
 int main()
 {
-  uint32_t volatile start = 0;
   GPIO_Init();
 
-  if(start)
-  {
-    green_main();
-  }
-  else
-  {
-    red_main();
-  }
+  // stack initialize for green_main thread
+  *(--sp_green)   = (1 << 24);               // xPSR should be 1 to tell the CPU it executing in thumb mode
+  *(--sp_green)   = (uint32_t)&green_main;  // PC
+  *(--sp_green)   = (uint32_t)0x0000000DU;  // LR
+  *(--sp_green)   = (uint32_t)0x0000000EU;  // R12
+  *(--sp_green)   = (uint32_t)0x0000000EU;  // R3
+  *(--sp_green)   = (uint32_t)0x0000000EU;  // R2
+  *(--sp_green)   = (uint32_t)0x0000000AU;  // R1
+  *(--sp_green)   = (uint32_t)0x0000000DU;  // R0
+
+   // stack initialize for red_main thread
+  *(--sp_red)   = (1 << 24);               // xPSR should be 1 to tell the CPU it executing in thumb mode
+  *(--sp_red)   = (uint32_t)&red_main;  // PC
+  *(--sp_red)   = (uint32_t)0x0000000BU;  // LR
+  *(--sp_red)   = (uint32_t)0x0000000EU;  // R12
+  *(--sp_red)   = (uint32_t)0x0000000EU;  // R3
+  *(--sp_red)   = (uint32_t)0x0000000EU;  // R2
+  *(--sp_red)   = (uint32_t)0x0000000EU;  // R1
+  *(--sp_red)   = (uint32_t)0x0000000FU;  // R0
+
   while(1)
-  {
-  }
           /*this inifinite loop I brought these
           unctions here I had to keep the while
           one in the original main function in order to
           make the decode not terminate.But when we put it
           here it remains and it uses time to switch in between the blue main and then the
           orange moon*/
+
+  //If we don't put this wire one here this main function is going to run and it's going to terminate.
+  {
+  }
 }
 
 void GPIO_Init(void)
@@ -107,7 +126,7 @@ void blueOff(void)
 
 void orangeOn(void)
 {
- GPIOA->ODR |= ORANGE;
+  GPIOA->ODR |= ORANGE;
 }
 
 void orangeOff(void)
@@ -117,7 +136,7 @@ void orangeOff(void)
 
 
 void redOn(void)
-{
+{ 
   GPIOA->ODR |= RED;
 }
 
@@ -135,9 +154,6 @@ void greenOff(void)
 {
   GPIOA->ODR &= ~GREEN;
 }
-
-
-
 
 int red_main()
 {
